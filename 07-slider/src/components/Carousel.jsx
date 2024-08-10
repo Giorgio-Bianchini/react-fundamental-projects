ffe809f1017ba1fe85da7929eb63b8c9c7a68660
Giorgio Bianchini <gio.whiteh@gmail.com>
2024-08-10 22:13:37 +0200

    Final touches

---
+++ 07-slider/src/components/Carousel.jsx
@@ -0,0 +1,34 @@
+import { shortList, list, longList } from "../data";
+import { useState } from "react";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+  const prevSlide = () => {};
+  const nextSlide = () => {};
+
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button className="prev" type="button" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button className="next" type="button" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+export default Carousel;
