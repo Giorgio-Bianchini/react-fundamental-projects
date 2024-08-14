3d4b885bb95178cbfdb8d7ff24d8f7115cb1d09b
Giorgio Bianchini <gio.whiteh@gmail.com>
2024-08-14 18:36:11 +0200

    final commit

--- 09-color-generator/src/components/ColorList.jsx
+++ 09-color-generator/src/components/ColorList.jsx
@@ -1,6 +1,13 @@
-const ColorList = () => {
+import SingleColor from "./SingleColor";
+import { nanoid } from "nanoid";
+
+const ColorList = ({ colors }) => {
   return (
-    <div>ColorList</div>
-  )
-}
-export default ColorList
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
+};
+export default ColorList;
--- 09-color-generator/src/components/SingleColor.jsx
+++ 09-color-generator/src/components/SingleColor.jsx
@@ -1,6 +1,28 @@
-const SingleColor = () => {
+import { toast } from "react-toastify";
+
+const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to the clipboard");
+      } catch (error) {
+        toast.error("Failed to copy to the clipboard");
+      }
+    } else {
+      toast.error("Clipboard access not available");
+    }
+  };
   return (
-    <div>SingleColor</div>
-  )
-}
-export default SingleColor
+    <article
+      onClick={saveToClipboard}
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
+};
+export default SingleColor;
